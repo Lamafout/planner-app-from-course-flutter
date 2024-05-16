1160c6bedd6a2ed43b2ae1ecee31e5f64dbda205
lamafout <dzordzvashington@gmail.com>
2024-05-16 22:52:55 +0300

    First step to getting mobile's IP to backend works

--- lib/handler/handler.dart
+++ lib/handler/handler.dart
@@ -1,10 +1,15 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:wifi_info_flutter/wifi_info_flutter.dart';
+
 import '../classes/transaction.dart';
 
 class PostSend {
   void postData({required Transaction transaction, required Function doSomething}) async {
-    var url = Uri.parse('http://127.0.0.1:8080');
+    final currentWifiIP = await getIP();
+    var url = Uri.parse('http://' + currentWifiIP + ':8080');
+    //10.80.175.187
     
     var data = {
       'id': transaction.id.toString(),
@@ -23,6 +28,18 @@ class PostSend {
       doSomething();
     } else {
       print('Error: ${response.statusCode}');
+    }
+  }
+
+  Future<String> getIP() async{
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.wifi){
+      var wifiIP = await (WifiInfo().getWifiIP());
+      return wifiIP ?? '';
+    }
+    // TODO добавить возможность подключиться к другому айпишнику в случае, если девайс не подключёг к вай-фаю
+    else {
+      return '';
     }
   }
 }
